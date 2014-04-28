b3eae535ca13147df0e263d92068ebd4c4699d6f
Milind Ganjoo <mganjoo@stanford.edu>
2014-04-28 15:12:16 -0700

    Streamline code

--- train.m
+++ train.m
@@ -8,17 +8,20 @@ numCategories = 5;  % 5 emotion categories
 
 % Parameters
 trainParams.lambda = 0.1;          % Regularization parameter
-trainParams.maxIter = 500;         % Maximum number of iterations
 trainParams.f = @tanh;             % Function to use in the neural network activations
 trainParams.f_prime = @tanh_prime; % Derivative of f
 
 % Initialize actual weights
-disp('Initializing parameters');
 [ theta, decodeInfo ] = initializeParameters(size(X, 1), numCategories);
 trainParams.decodeInfo = decodeInfo;
 
-dataToUse.feats = X;
-dataToUse.categories = Y;
-theta = trainLBFGS(trainParams, dataToUse, theta);
+% minFunc options
+options.Method = 'lbfgs';
+options.display = 'off';
+options.MaxIter = 500;    % Maximum number of iterations
+
+data.feats = X;
+data.categories = Y;
+[theta, ~, ~, ~] = minFunc( @(p) softmaxCost(p, data, trainParams ), theta, options);
 
 save('params.mat', 'theta', 'decodeInfo');
